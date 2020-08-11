365eb4c8de1bab38b71ebe6b998f7792c049325d
T Jacobs <QuarterT@users.noreply.github.com>
2020-08-11 13:38:56 -0700

    adding else, and bug fixing

--- Dry Mancala.js
+++ Dry Mancala.js
@@ -13,44 +13,44 @@ p1P1 = board[5]; p2P6 = board[6];
 */
 function p1P6 (){
     if (board[0] < 6){
-    if (board[0] === 5) {
-        pit6_4();
-        if (board[5] = 0){
+       if (board[0] === 5) {
+            pit6_4();
+            if (board[5] = 0){
             store1 = store1 + board[6];
-        } else board[5]++;
-    }
-    if (board[0] === 4) {
+            } else board[5]++;
+        }
+        else if (board[0] === 4) {
             board[1]++;
             board[2]++;
             board[3]++;
-        if (board[4] = 0){
-            store1 = store1 + board[7];
-        } else board[4]++;
-    }
-    if (board[0] === 3) {
-        board[1]++;
-        board[2]++;
-        if (board[3] = 0){
-            store1 = store1 + board[8];
-        } else board[3]++;
-    }
-    if (board[0] === 2) {
-        board[1]++;
-        if (board[2] = 0){
-            store1 = store1 + board[9];
-        } else board[2]++;
-    }
-    if (board[0] === 1) {
-        if (board[1] = 0){
-            store1 = store1 + board[10];
-        } else board[2]++;
-    }   
-    }
-    else if (board[0] > 6) {
-    pit6_6();
-    if (board[0] === 7) {
-        board[7]++;
-    }
+            if (board[4] = 0){
+                store1 = store1 + board[7];
+            } else board[4]++;
+        }
+        else if (board[0] === 3) {
+            board[1]++;
+            board[2]++;
+            if (board[3] = 0){
+                store1 = store1 + board[8];
+            } else board[3]++;
+        }
+        else if (board[0] === 2) {
+            board[1]++;
+            if (board[2] = 0){
+                store1 = store1 + board[9];
+            } else board[2]++;
+                }
+        else if (board[0] === 1) {
+            if (board[1] = 0){
+                store1 = store1 + board[10];
+            } else board[2]++;
+                }   
+            }
+        else if (board[0] > 6) {
+            pit6_6();
+            if (board[0] === 7) {
+                board[7]++;
+            }
     else if (board[0] === 8) {
         board[7]++;
         board[8]++;
