efe9455b57a9776e56b9ced4fcc9743491f52789
T Jacobs <tarynljacobs@gmail.com>
2020-08-10 20:40:04 -0700

    working on dry

--- Dry Mancala.js
+++ Dry Mancala.js
@@ -1,19 +1,17 @@
 //dryer attempt
-let board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
+let store1 = 0;
+let store2 = 0;
+let board = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
 /* P = player, p# = pit number
-                 store2 = board[13]
-p1P6 = board[0]; p2P1 = board[12]
-p1p5 = board[1]; p2P2 = board[11]
-p1P4 = board[2]; p2P3 = board[10];
-p1P3 = board[3]; p2P4 = board[9];
-p1P2 = board[4]; p2P5 = board[8];
-p1P1 = board[5]; p2P6 = board[7];
-store1 = board[6]
+p1P6 = board[0]; p2P1 = board[11]
+p1p5 = board[1]; p2P2 = board[10]
+p1P4 = board[2]; p2P3 = board[9];
+p1P3 = board[3]; p2P4 = board[8];
+p1P2 = board[4]; p2P5 = board[7];
+p1P1 = board[5]; p2P6 = board[6];
 */
-
-p2P4 = board(8);
-*/
-if (board[0] < 6){
+function p1P1 (){
+    if (board[0] < 6){
     if (board[0] === 5) {
         pit6_4();
         if (board[5] = 0){
@@ -46,8 +44,8 @@ if (board[0] < 6){
             store1 = store1 + board[10];
         } else board[2]++;
     }   
-}
-else if (board[0] > 6) {
+    }
+    else if (board[0] > 6) {
     pit6_6();
     if (board[0] === 7) {
         board[7]++;
@@ -56,15 +54,32 @@ else if (board[0] > 6) {
         board[7]++;
         board[8]++;
     }
-}
-    
-
-else if (board[0] === 6) {
-    pit6_6();
-}   
-else if (board[]= 0{
-        //play sad tune 
-}
+    else if (board[0] === 9) {
+        board[7]++;
+        board[8]++;
+        board[9]++;
+    }
+    else if (board[0] === 10) {
+        board[7]++;
+        board[8]++;
+        board[9]++;
+        board[10]++;
+    }
+    else if (board[0] === 11) {
+        board[7]++;
+        board[8]++;
+        board[9]++;
+        board[10]++;
+        board[11]++;
+    }  
+    }   
+    else if (board[0] === 6) {
+        pit6_6();
+    }   
+    else (board[0]= 0) {
+        
+    }
+};
 
 function pit6_4(){
     board[1]++;
